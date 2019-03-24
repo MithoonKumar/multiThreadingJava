9b9f09ba206f885e2f931b8ec23c8163b159f019
Mithoon Kumar <mithoonk@zeta.tech>
2019-03-24 23:10:40 +0530

    Implement synchronization

--- src/UtilityClass.java
+++ src/UtilityClass.java
@@ -1,5 +1,5 @@
 public class UtilityClass {
-    public void printUsingLoop(String threadName, Integer n) {
+    synchronized public void printUsingLoop(String threadName, Integer n) {
         for(Integer i = 0; i<n; i++) {
             System.out.println("The name of the printer thread is " + threadName);
         }
